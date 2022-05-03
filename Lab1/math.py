e701391f6a1e9d6de9b32c5be443513afd0a8730
Matdrox <Matdrox@users.noreply.github.com>
2022-05-03 10:06:00 +0200

    Organized

--- Lab1/math.py
+++ Lab1/math.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 
 a = np.arange(1, 6, 1)
 b = np.arange(0, 2*np.pi, 0.1)
-c = np.matrix([np.arrange(1, 6, 1)])
-print(a)
-print(b)
-print(c)
+c = np.zeros(shape=(3, 2))
+c[0] = [1, 2]
+c[1] = [3, 4]
+c[2] = [5, 6]
+print(a,'\n')
+print(b,'\n')
+print(c,'\n')
