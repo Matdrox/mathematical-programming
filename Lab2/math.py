d3fe9d479a596691d164b32099dad02556523c54
Matdrox <Matdrox@users.noreply.github.com>
2022-05-15 13:58:32 +0200

    Setup Lab2

---
+++ Lab2/math.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+val = int(input('Välj uppgiften du ska kolla på (int): '))
+functions = [uppgift1, uppgift2, uppgift3, uppgift4,
+             uppgift5]
+functions[val-1]()
