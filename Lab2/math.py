bb15b92f1c55125d41f1215402d6f325acd96865
Matdrox <Matdrox@users.noreply.github.com>
2022-05-17 20:48:21 +0200

    Updated Lab2

--- Lab2/math.py
+++ Lab2/math.py
@@ -1,5 +1,7 @@
 import numpy as np
+import scipy.ndimage as sp
 import matplotlib.pyplot as plt
+from PIL import Image
 
 
 def uppgift1():
@@ -19,7 +21,6 @@ def uppgift1():
             #     break
             temp += 1
 
-
     # a = np.arange(-2, 2.01, 0.01)
     # b = np.arange(-2, 2.01, 0.01)
     M = np.zeros(shape=(401, 401))
@@ -37,7 +38,7 @@ def uppgift1():
                 temp = mandelbrot(complex(i, j))
                 if temp:
                     M[int((i+2)*100), int((j+2)*100)] = 1
-        
+
         plt.imshow(M.T, cmap='gray', extent=(a_min, a_max, b_min, b_max))
         plt.show()
 
@@ -54,13 +55,56 @@ def uppgift1():
                 temp = mandelbrot(complex(i, j))
                 if temp:
                     M[int((i+1)*1000), int((j)*1000)] = 1
-        plt.imshow(N.T, cmap='gray', extent=(a_min, a_max, b_min, b_max))
+        plt.imshow(M.T, cmap='gray', extent=(a_min, a_max, b_min, b_max))
         plt.show()
 
     full()
     # zoom()
 
 
+def uppgift2():
+    img = plt.imread('borggarden_small.jpg')
+    R, G, B = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+    img_grayscale = R * 0.2989 + G * 0.5870 + B * 0.1140
+
+    Gx = np.array([[-1, 0, 1],
+                      [-2, 0, 2],
+                      [-1, 0, 1]])
+
+    Gy = np.array([[-1, -2, -1],
+                      [0, 0, 0],
+                      [1, 2, 1]])
+
+    xd = sp.convolve(img_grayscale, Gx, mode='constant')
+    yd = sp.convolve(img_grayscale, Gy, mode='constant')
+
+    rows, cols = img_grayscale.shape
+
+    S = np.zeros((rows, cols))
+    for i in range(rows):
+        for j in range(cols):
+            S[i][j] = np.sqrt(np.square(xd[i][j]) + np.square(yd[i][j]))
+
+    S[S>250] = 0
+    S[S<50] = 255
+
+    n = 1
+    M = np.ones((n, n))
+    result = sp.convolve(S, M, mode='constant')
+
+    for i in range(n):
+        for j in range(n):
+            if result[i][j] < 255*n*n:
+                result[i][j] = 255
+            else:
+                result[i][j] = 0
+
+    result[result>255*n*n] = 0
+    result[result<255*n*n] = 0
+
+    plt.imshow(result, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+
 val = int(input('Välj uppgiften du ska kolla på (int): '))
-functions = [uppgift1]
+functions = [uppgift1, uppgift2]
 functions[val-1]()
